73e510c98932692f4274a7597e090d6bd111189d
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2021-08-27 16:10:29 +0900

    웹팩 로더 사용하기
    인프런 '웹 게임을 만들며 배우는 Vue' 3-3강 수강 완료

---
+++ 3.숫자야구/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+// ES2015 에  들어 있는 모듈 시스템. 나는 package.json에 기록했던 Vue를 사용하겠다.
+
+import NumberBaseball from './NumberBaseball.vue'
+//  import로 NumberBaseball 연결
+
+new Vue(NumberBaseball).$mount( '#root' );
+//  el 과 같은 역할. vue가 통제할 태그
+// vue 컴포넌트를 인스턴스에 연결해 주어야 한다.
