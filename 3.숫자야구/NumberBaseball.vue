72f7e5387cbd0ee21efdbb773fe1defa1ae0920f
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2021-08-28 20:51:45 +0900

    v-for로 반복문 사용하기
    인프런 '웹 게임을 만들며 배우는 Vue' 3-3강 수강 완료

--- 3.숫자야구/NumberBaseball.vue
+++ 3.숫자야구/NumberBaseball.vue
@@ -4,24 +4,43 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <form v-on:submit="onSubmitForm">
-            <input ref="answer" maxlength="4" v-model="value">
-            <button>입력</button>
+        <form v-on:submit.prevent="onSubmitForm">
+            <!-- v-on: 은 @로 대체할 수 있다. -->
+            <!-- preventDefault() 를 사용하기 귀찮으면 .prevent 장식자를 붙여주면 된다. -->
+            <input ref="answer" minlength="4" maxlength="4" v-model="value">
+            <button type="submit">입력</button>
         </form>   
-        <div>시도: {{}}</div>
+        <div>시도: {{tries.length}}</div>
+        <!-- 배열의 갯수 표시 -->
+
+            <li v-for="t in tries">
+                <!-- try는 예약어라서 쓸 수 없음. -->
+                <div>{{t.try}}</div>
+                {<div>{{t.result}}</div>
+            </li>
+            <!-- v-for를 이용해 배열의 내용을 반복해서 써 줌 -->
+        </ul>
     </div>
 </template>
 <script>
     export default {
         data(){
             return{
+                tries: [], // 시도.  '입력' 버튼을 누를 때마다  배열에 어떤 시도를 했는지 push로 기록한다.
                 value: '',
                 result: ''
             }
         },
         methods: {
             onSubmitForm(e){
-                e.preventDefault();
+                // e.preventDefault();
+                this.tries.push({
+                    try: this.value,
+                    result: '',
+                });
+                // tries 안에 객체 넣기
+                this.value = '';
+                this.$refs.answer.focus();
             }
         }
     }; 
