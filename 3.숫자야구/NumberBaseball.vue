20fc1f58a3076f8e38c6cdcf9b528660e91d2dc2
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2021-08-30 12:12:39 +0900

    숫자야구 완성
    인프런 '웹 게임을 만들며 배우는 Vue' 3-5강 수강 완료

--- 3.숫자야구/NumberBaseball.vue
+++ 3.숫자야구/NumberBaseball.vue
@@ -16,17 +16,30 @@
             <li v-for="t in tries">
                 <!-- try는 예약어라서 쓸 수 없음. -->
                 <div>{{t.try}}</div>
-                {<div>{{t.result}}</div>
+                <div>{{t.result}}</div>
             </li>
             <!-- v-for를 이용해 배열의 내용을 반복해서 써 줌 -->
         </ul>
     </div>
 </template>
 <script>
+// 데이터나 화면과 크게 관련이 없기 때문에, 굳이 methods에 적지 않음. 다른 컴포넌트에서도 쓸 수 있음.
+    const getNumbers = () =>{
+        const candidates = [1,2,3,4,5,6,7,8,9];
+        const array = [];
+        for (let i = 0; i < 4; i+=1){
+            const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            array.push(chosen);
+        }
+        return array;
+    }
+
     export default {
+        // export default 한 객체를 main.js에서 import 로 불러온다.
         data(){
             return{
-                tries: [], // 시도.  '입력' 버튼을 누를 때마다  배열에 어떤 시도를 했는지 push로 기록한다.
+                answer: getNumbers(), // 랜덤한 숫자를 정답으로 지정. ex) [1,2,3,4]
+                tries: [], // 시도.  '입력'  버튼을 누를 때마다  배열에 어떤 시도를 했는지 push로 기록한다.
                 value: '',
                 result: ''
             }
@@ -34,13 +47,55 @@
         methods: {
             onSubmitForm(e){
                 // e.preventDefault();
-                this.tries.push({
+                if (this.value ===  this.answer.join('')){ //value 는 문자열이지만 answer는 배열이다.
+                    //  정답을 맞췄으면
+                    this.tries.push({
+                        // tries 안에 객체 넣기
                     try: this.value,
-                    result: '',
+                    result: '홈런',
                 });
-                // tries 안에 객체 넣기
+                this.result = '홈런';
+                alert('게임을 다시 실행합니다.');
+                // 초기화
                 this.value = '';
+                this.answer = getNumbers();
+                this.tries= [];
                 this.$refs.answer.focus();
+                }
+                else{
+                    // 정답을 틀렸을 때
+                    if(this.tries.length >= 9){ // 10번째 시도
+                        this.result = `10번 넘게 틀려서 실패! 답은 ${this.answer.join(',')} 였습니다!`
+                        alert('게임을 다시 시작합니다.');
+                        this.value = '';
+                        this.answer = getNumbers();
+                        this.tries = [];
+                        this.$refs.answer.focus();
+
+                    }
+                    else{
+
+                    }
+                    let strike = 0;
+                    let ball = 0;
+                    const answerArray = this.value.split('').map(v => parseInt(v)); // 문자열을 숫자 배열로 바꾸는 코드. 배열을 순회하며 요소를 정수화하여 새로운 배열로 반환.
+
+                    for (let i = 0; i<4; i++){
+                        if (answerArray[i] === this.answer[i]){ // 숫자 자릿수 모두 정답
+                            strike++;
+                        }
+                        else if (this.answer.includes(answerArray[i])) { // 숫자만 정답,  includes는 배열이 특정 요소를 포함하고 있는지 판별
+                            ball++;
+                        }
+                    }
+
+                    this.tries.push({
+                        try : this.value,
+                        result : `${strike} 스트라이크, ${ball} 볼`
+                    });
+                     this.value = '';
+                     this.$refs.answer.focus();
+                }
             }
         }
     }; 
