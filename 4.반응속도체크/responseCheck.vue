8ddcf8aed4bfb532248dfe6f7d424a94df8b22a7
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2021-09-02 14:32:08 +0900

    반응속도체크와 webpack watch
    인프런 '웹 게임을 만들며 배우는 Vue' 4-1강 수강 완료

---
+++ 4.반응속도체크/responseCheck.vue
@@ -0,0 +1,37 @@
+// 확장자는 vue 지만 자바스크립트 파일!
+// 뷰 컴포넌트가 바로 Vue 파일!
+
+<template>
+    <div>
+        <div id="screen">
+            {{message}}
+            <!-- 사용자가 클릭하는 화면 -->
+        </div>
+        <div>
+            <div>평균 시간: {{}}</div>
+            <button v-on:click="onReset">리셋</button>
+        </div>
+    </div>
+</template>
+<script>
+
+
+    export default {
+    
+        data(){
+            return{
+                result: [],
+                state: 'waiting',
+                message: '클릭해서 시작하세요.',
+            }
+        },
+        methods: {
+            onReset: function(){
+
+            }
+        }
+    }; 
+</script>
+<style>
+
+</style>
