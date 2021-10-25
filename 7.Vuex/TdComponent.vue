9369758c2502bc8af175aada88a5b9ff2069b422
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2021-10-25 23:48:29 +0900

    Vuex Mutations
    인프런 '웹 게임을 만들면서 배우는 Vue' 7-7강

--- 7.Vuex/TdComponent.vue
+++ 7.Vuex/TdComponent.vue
@@ -2,7 +2,7 @@
     <td v-on:click="onClickTd">{{cellData}}</td>
 </template>
 <script>
-    import EventBus from './EventBus.js';
+    import {CHANGE_TURN, CLICK_CELL, NO_WINNER, RESET_GAME, SET_WINNER} from './store.js'; // mutation 을 변수명으로 빼둔 이유
 
     export default{
         props: {
@@ -20,9 +20,49 @@
                 if(this.cellData){
                     return; // 남이 이미 누른 칸에는 return 으로 함수 중단하기.
                 }
-                EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
-                // clickTd 이벤트를 명시적으로 실행시킨다.
-                // this.cellIndex와 this.rowIndex는 부모 컴포넌트에서 props로 받았는데, $emit을 통해 넘겨줄 수 있다.
+            // this.$store.commit('CLICK_CELL');
+            this.$store.commit(CLICK_CELL, {row: this.rowIndex, cell : this.cellIndex});
+            //mutation 을 부를 때는 commit 을 사용하여 불러온다.  mutition 이름이 CLICK_CELL 인 것을 실행한다.
+
+           let win = false;
+                if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2]){
+                    win = true;
+                }
+                if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn){
+                    win = true;
+                }
+                if (rootData.tableData[0][0] ===  rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn){
+                    win = true;
+                }
+                if (rootData.tableData[0][2] ===  rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn){
+                    win = true;
+                }
+                if (win){
+                    // 이긴 경우 : 초기화
+                    this.$store.commit(SET_WINNER, this.turn);
+                    this.$store.commit(RESET_GAME);
+                    // 두 번째 인자에 데이터를 넣을 수 있다.
+                } else{
+                    let all = true; //  all이 true 면 무승부 라는 뜻
+                    rootData.tableData.forEach((row) => { // 모든 칸이 다 차 있는지 검사하기!
+                        // 무승부 검사
+                        row.forEach((cell)=>{
+                            if (!cell){
+                                all = false;
+                            }
+                        });
+                    });
+                    if (all){
+                        // 무승부인 경우
+                        this.$store.commit(NO_WINNER);
+                        this.$store.commit(RESET_GAME);
+                    } 
+                    else{
+                        // 게임이 안 끝났을 때는 턴을 다음 사람 턴으로
+                        this.$store.commit(CHANGE_TURN);
+                    }
+                }
+
             }
         },
         computed:{
--- 7.Vuex/store.js
+++ 7.Vuex/store.js
@@ -1,14 +1,56 @@
+import Vue from 'vue';
 import Vuex from 'vuex';
 
+export const SET_WINNER = 'SET_WINNER'; // 함수명을 변수로 빼 줄 수 있다. (ES2015 문법). 동적 속성
+export  const CLICK_CELL = 'CLICK_CELL';
+export  const CHANGE_TURN = 'CHANGE_TURN';
+export  const RESET_GAME = 'RESET_GAME';
+export  const NO_WINNER = 'NO_WINNER';
+// export하여 다른 파일에서 쓸 수 있게끔 모듈로 만든다.
+
 export default new Vuex.Store({
-    state:{
+    state: {
         // vue의 data와 비슷
+        tableData: [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+            // 2차원 배열: 배열 안에 배열이 또 들어가 있음!
+        ],
+        turn: 'O', // 클릭할 때마다 턴이 O에서 X로, X에서 O로 넘어감.
+        winner: '',
     },
     getters: {
         // vue의 computed와 비슷
     },
     mutations: {
         // state를 동기적으로 수정할 때 사용
+        [SET_WINNER](state, winner){
+            state.winner = winner;
+        },
+    [CLICK_CELL](state,{row, cell}){
+            // 첫 번째 매개변수에는 state가, 두 번째 매개변수에는 data가 들어간다.
+            Vue.$set(state.tableData[row],cell,state.turn);
+            // 칸에 O나 X 그려주기. tableData[row][cell] 에 state.turn을 대입한다.
+            //Vuex에서도 배열의 인덱스에 접근하는 경우에는 화면이 바뀌지 않기 때문에 Vue.set이나 this.$set()을 사용해 주어야 한다.
+            // Vuex에는 $set 이 없으므로 Vue.set 사용.
+        },
+        [CHANGE_TURN](state){
+            state.turn = state.turn === 'O' ? 'X' : 'O'; // turn이 O였으면 X로, X였으면 O로 변경
+        },
+        [RESET_GAME](state){
+            state.tableData =  [
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', '']
+            ];
+            state.turn =  'O'; 
+            state.winner = '';
+        },
+        [NO_WINNER](state){
+            state.winner = '';
+        }
+        // 이것들은 다 onClickTd 안에 들어있던 것들이다.
     },
     actions: {
         // 비동기를 사용할 때, 또는 여러 뮤테이션을 연달아 실행할 때
